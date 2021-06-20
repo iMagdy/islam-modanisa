3d06000429d8d0252a5b8851b50952ab9bfa0f47
Islam Magdy <i.magdy.m@gmail.com>
2021-06-20 22:14:55 +0200

    Test::Component - Fixed TodoList

--- web/src/components/TodoList.vue
+++ web/src/components/TodoList.vue
@@ -1,10 +1,20 @@
 <template>
-  <div></div>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item.text }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'TodoList',
+  props: {
+    items: Array,
+    required: true,
+    validator: (value) => value.every(({ id, text }) => 
+      typeof text === 'string' && typeof id === 'string' )
+  }
 }
 </script>
 
--- web/tests/unit/components/todo-list.spec.js
+++ web/tests/unit/components/todo-list.spec.js
@@ -3,19 +3,25 @@ import TodoList from '@/components/TodoList.vue'
 
 describe('TodoList.vue', () => {
   it('can list todo items', () => {
-    const wrapper = shallowMount(TodoList, {
-      propsData: {
-        items: [
-          'buy some milk',
-          'buy some mango'
-        ]
+    const items = [
+      {
+        id: '1',
+        text: 'buy some milk',
+      },
+      {
+        id: '2',
+        text: 'buy some mango',
       }
-    });
+    ];
+    const wrapper = shallowMount(TodoList, { propsData: { items } });
 
     const listElement = wrapper.find('ul');
     const listItems = wrapper.findAll('ul li');
 
     expect(listElement.exists()).toBe(true);
     expect(listItems.exists()).toBe(true);
+    items.forEach((item, i) => {
+      expect(listItems.at(i).text()).toEqual(item.text);
+    })
   });
 })
