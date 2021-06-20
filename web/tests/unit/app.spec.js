ff81db446e90a3e04fb1be28b0cd4afbd4a949f1
Islam Magdy <i.magdy.m@gmail.com>
2021-06-20 23:02:28 +0200

    Test::Unit - Submit a new todo to the API

--- web/tests/unit/app.spec.js
+++ web/tests/unit/app.spec.js
@@ -1,13 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
-import App from '@/App.vue'
-
-// When I write "buy some milk" to <text box> and click on <add button>
-// Then I should see item in ToDo list 
+import { shallowMount } from '@vue/test-utils';
+import sinon from 'sinon';
+import App from '@/App.vue';
 
 describe('App.vue', () => {
-  it('renders application title and core components', () => {
-    const wrapper = shallowMount(App);
+  let wrapper;
+  let postTODOSpy;
+  const sandbox = sinon.createSandbox();
+  
+  beforeEach(() => {
+    postTODOSpy = sandbox.spy(App.methods, 'postTODO');
+    wrapper = shallowMount(App);
+  });
 
+  afterEach(() => {
+    sandbox.restore();
+  });
+  
+  it('renders application title and core components', () => {
     const title = wrapper.find('h1');
 
     const inputComponent = wrapper.findComponent({ name: 'TodoInput' });
@@ -16,5 +25,18 @@ describe('App.vue', () => {
     expect(title.text()).toMatch('Modanisa Todos');
     expect(inputComponent.exists()).toBe(true);
     expect(listComponent.exists()).toBe(true);
+  });
+
+  it('can receive "modanisa.createTODO" and triggers postTODO method to dispatch XHR to the API', async () => {
+    window.fetch = () => true;
+    
+    wrapper.vm.$emit('modanisa.createTODO', 'My test todo item');
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted()['modanisa.createTODO'][0]).toEqual([ 'My test todo item' ]);
+
+    expect(postTODOSpy.callCount).toEqual(1);
+    expect(postTODOSpy.getCall(0).args[0]).toEqual('My test todo item');
   })
 })
