6b3457167beab2c83b2e1bd7482c5ce23f1fe79d
Islam Magdy <i.magdy.m@gmail.com>
2021-06-20 12:42:01 +0200

    First test for the main app component

--- web/tests/unit/app.spec.js
+++ web/tests/unit/app.spec.js
@@ -1,8 +1,11 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
+// When I write "buy some milk" to <text box> and click on <add button>
+// Then I should see item in ToDo list 
+
 describe('App.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders application title and core components', () => {
     const wrapper = shallowMount(App);
 
     const title = wrapper.find('h1');
@@ -10,6 +13,7 @@ describe('App.vue', () => {
     const inputComponent = wrapper.findComponent({ name: 'TodoInput' });
     const listComponent = wrapper.findComponent({ name: 'TodoList' });
     
+    expect(wrapper.name()).toMatch('ModanisaApp');
     expect(title.text()).toMatch('Modanisa Todos');
     expect(inputComponent.exists()).toBe(true);
     expect(listComponent.exists()).toBe(true);
