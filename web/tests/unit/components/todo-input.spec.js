f7740ba728651c0ac8016a2d584cea7fcfec36c4
Islam Magdy <i.magdy.m@gmail.com>
2021-06-20 13:06:28 +0200

    Test::Business - TodoInput

--- web/tests/unit/components/todo-input.spec.js
+++ web/tests/unit/components/todo-input.spec.js
@@ -13,4 +13,28 @@ describe('TodoInput.vue', () => {
     expect(inputElement.exists()).toBe(true);
     expect(buttonElement.exists()).toBe(true);
   });
+
+  it('can type new todo & trigger event "modanisa.createTODO" by clicking on the submit button', () => {
+    const wrapper = shallowMount(TodoInput);
+
+    const inputElement = wrapper.find('input[type="text"]');
+    const buttonElement = wrapper.find('button[type="submit"]');
+
+    inputElement.setValue('buy some milk');
+    buttonElement.trigger('click');
+
+    expect(wrapper.emitted()['modanisa.createTODO'][1]).toEqual(['buy some milk']);
+  });
+  
+  it('can type new todo & trigger event "modanisa.createTODO" by hitting enter', () => {
+    const wrapper = shallowMount(TodoInput);
+
+    const inputElement = wrapper.find('input[type="text"]');
+
+    inputElement.setValue('buy some milk');
+
+    inputElement.trigger('keydown.enter');
+
+    expect(wrapper.emitted()['modanisa.createTODO'][1]).toEqual(['buy some milk']);
+  });
 })
