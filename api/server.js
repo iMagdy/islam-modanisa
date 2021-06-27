819ee06532dfcb9a41b410949bf4b9e98696fb60
Islam Magdy <i.magdy.m@gmail.com>
2021-06-28 01:28:54 +0200

    Add CORS headers

--- api/server.js
+++ api/server.js
@@ -4,9 +4,15 @@ const http = require('http');
 const db = new Map();
 
 const server = http.createServer((req, res) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'GET, POST',
+  };
   
   if (req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, headers);
     res.end(JSON.stringify([...db.entries()].map(([ id, data ]) => ({ id, ...data }))));
     return;
   }
@@ -18,19 +24,19 @@ const server = http.createServer((req, res) => {
 
     req.on('end', () => {
       if(body.length === 0) {
-        res.writeHead(422, { 'Content-Type': 'application/json' });
+        res.writeHead(422, headers);
         return res.end(JSON.stringify({ error: true, message: 'Incorrect TODO content. Must post a non-empty string' }));
       }
       
       const id = Math.random().toString(36).substr(2, 9);
       db.set(id, { text: body, createdAt: new Date().toISOString() });
-      res.writeHead(201);
+      res.writeHead(201, headers);
       res.end();
     });
     return;
   }
   
-  res.writeHead(405, { 'Content-Type': 'application/json' });
+  res.writeHead(405, headers);
   return res.end(JSON.stringify({ error: true, message: 'Unsupported method' }));
 });
 
